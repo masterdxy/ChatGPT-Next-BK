093d168e60d8be141d9d6ef68527e953fa1dca42
masterdxy <17710880510>
2023-11-30 17:43:34 +0800

    add

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          Memlab GPTs
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          内部使用
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
